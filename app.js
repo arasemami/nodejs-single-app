9a591e73d066df7f2f0fd678da992c92b1f2ddf9
aras armani <aras.armani2014@gmail.com>
2019-09-13 22:13:01 +0300

    fixed nemone

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', (req,res) => {
     res.send('hello world')
 });
 
+app.get('/aras', (req,res) => {
+    let a = { "namr":"aras","age":20 }
+    res.send(a)
+});
 
 
+// listening to the server 
 app.listen(3000);
