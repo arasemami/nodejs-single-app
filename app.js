cbeff2c0ee6f132b422c4ffc33216c698749fcac
aras armani <aras.armani2014@gmail.com>
2019-09-13 23:42:50 +0300

    dot env is aaded

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose')
+require('dotenv/config');
 
-app.get('/', (req,res) => {
-    res.send('hello world')
+// //Middleware
+// app.use('/post' , () => {
+//     console.log('middleware is runing');
+// })
+// Routs
+
+app.get('/', (req, res) => {
+    res.send('hello world kir be arase mos ')
 });
 
-app.get('/aras', (req,res) => {
-    let a = { "namr":"aras","age":20 }
+app.get('/post', (req, res) => {
+    let a = {
+        "namr": "aras",
+        "age": 20
+    }
     res.send(a)
 });
 
 
-// listening to the server 
+//connect to db
+mongoose.connect(
+    process.env.DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    },
+    () => {
+        console.log('mongo is connected')
+    }
+);
+
+// Listening to the server 
 app.listen(3000);
